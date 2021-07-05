30c2daa98de319da8663770efb433c1deb386528
Gabriel Lima <grl@icomp.ufam.edu.br>
2021-07-05 00:17:34 -0400

    feat: added table example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import * as jspreadsheet from 'jspreadsheet-ce';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'editor';
+  @ViewChild('deck') deck?: ElementRef<HTMLDeckgoDeckElement>;
+  async onSelected(evt: any) {
+    if (evt.detail) {
+      await this.deck?.nativeElement.blockSlide(true);
+    }
+  }
+
+  async addMedia() {
+    const currentIndex = await this.deck?.nativeElement.getActiveIndex();
+    const slides = await this.deck?.nativeElement.getLength();
+    console.log(`Current index: ${currentIndex}`);
+    console.log(`Total slides: ${slides}`);
+  }
+
+  ngAfterViewInit() {
+    console.log('After');
+    console.log(this.deck?.nativeElement.offsetWidth);
+  }
+  createTable(): void {
+    const tableContainer = document.createElement('div');
+    const drrContainer = this.createDrrContainer();
+    drrContainer.text = true;
+
+    const jReference = jspreadsheet(tableContainer, {
+      data: [[]],
+      columns: [
+        { type: "text", width: 100, },
+        { type: "text", width: 100, }
+      ],
+      minDimensions: [2, 2]
+    });
+    drrContainer.appendChild(tableContainer);
+
+    document.getElementById("slide1")?.appendChild(drrContainer);
+
+  }
+
+  private createDrrContainer() {
+    return document.createElement('deckgo-drr');
+  }
+
+
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { defineCustomElements as deckDeckGoCoreElement } from '@deckdeckgo/core/dist/loader';
+// import { defineCustomElements as deckDeckGoRevealElement } from '@deckdeckgo/reveal/dist/loader';
+// import { defineCustomElements as deckDeckGoSlideElement } from '@deckdeckgo/slide-title/dist/loader';
+import { defineCustomElements as deckDeckGoSlideAspectElement } from '@deckdeckgo/slide-aspect-ratio/dist/loader';
+import { defineCustomElements as deckDeckGoDrrElement } from '@deckdeckgo/drag-resize-rotate/dist/loader';
+import { defineCustomElements as deckDeckInlineEditorGoElement } from '@deckdeckgo/inline-editor/dist/loader';
+import { defineCustomElements as deckDeckGoLazyImgElement } from '@deckdeckgo/lazy-img/dist/loader';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
@@ -10,3 +16,11 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+deckDeckGoCoreElement();
+// deckDeckGoRevealElement();
+// deckDeckGoSlideElement();
+deckDeckGoSlideAspectElement();
+deckDeckGoDrrElement();
+deckDeckInlineEditorGoElement();
+deckDeckGoLazyImgElement();
